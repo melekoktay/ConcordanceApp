fef16268d12ce87ec5079029fcfe6eb1c17438bc
melekoktay <melekoktay@gmail.com>
2022-07-11 12:52:28 +0300

    Initial commit -- Maven dependency has been arranged for Eclipse -- JUnit integration has been done for unit testing

---
+++ concordance/src/main/java/concordance/ConcordanceMainApp.java
@@ -0,0 +1,14 @@
+package concordance;
+
+public class ConcordanceMainApp {
+
+	public static void main(String[] args) {
+		
+		if(args.length <= 0) {	
+			System.err.println("You must provide file for analyzing !");
+			System.exit(-1);
+		}
+
+	}
+
+}
---
+++ concordance/src/test/java/concordance/SimpleTest.java
@@ -0,0 +1,14 @@
+package concordance;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleTest {
+
+	@Test
+	public void test() {
+		System.out.println("Hello first unit test with JUnit 4");
+	}
+
+}
